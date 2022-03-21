d234a5280912dbab5c795bce1483cfcd23cab747
DamarisTeoc <teoc.918@gmail.com>
2022-03-21 12:38:40 +0100

    models

--- src/Controllers/LoversController.php
+++ src/Controllers/LoversController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controllers;
 
-
+use App\Models\Lovers;
 
 class LoversController{
 
--- src/Models/Lovers.php
+++ src/Models/Lovers.php
@@ -2,6 +2,49 @@
 
 namespace App\Models;
 
+use App\Database;
+
 class Lovers{
     
+    private int $id; 
+    private string $people;
+    private string $type;
+    private string $activity;
+    private string $date_time;
+
+    private $database;
+    private $table= 'citas';
+    public function __construct(){
+        if (!$this->database ) {
+            $this->database= new Database;
+
+        }
+
+      
+
+    }
+     
+    public function all(){
+
+        $query = $this->database->mysql->query("select * from {$this->table}");
+        $lovers_array = $query->fetchAll();
+
+        $result_array=[];
+
+        foreach ($lovers_array as $lovers) {
+            $lover_item = new Lovers($lovers ["id"], $lovers["people"],$lovers["type"],$lovers["activity"],$lovers["database"]);
+            array_push($result_array,$lover_item);
+
+            
+
+
+        }
+
+        return $result_array;
+
+    } 
+
+
+
+
 }
