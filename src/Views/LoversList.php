571e9c970ef71f375f916a98c4ceebdd0bae827b
Hushino <abde95.inf3@gmail.com>
2022-03-22 10:20:18 +0100

    TAG

--- src/Views/LoversList.php
+++ src/Views/LoversList.php
@@ -1,8 +1,29 @@
 <html>
     <?php
         require_once("components/layout.php");
+        
     ?>
     <body>
+        <?php
+    require_once("components/header.php");
+    ?>
         <h1>Works</h1>
+        <?php
+        foreach ($data ["lover"] as $lover){
+            echo <<<TAG
+            <article class="col-sm d-flex align-space-around justify-content-center">
+                <div class="card text-center" style="width: 15rem">
+                    <h3 class="card-title">{$lover->get_people()}</h3>
+                    <div class="card-body">
+                        <h5 class="card-title">{$lover->get_type()}</h5>
+                        <h6 class="card-text">{$lover->get_activity()}</h6>
+                        <p class="card-text">{$lover->get_date_time()}</p>
+                        <p class="card-text">{$lover->get_id()}</p>
+                    </div>
+                </div>
+            </article>
+            TAG;
+        }
+        ?>
     </body>
 </html>
