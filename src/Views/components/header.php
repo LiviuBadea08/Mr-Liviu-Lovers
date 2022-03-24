ea33103f8b7d0d274ea82a65f35c991168f4377d
Hushino <abde95.inf3@gmail.com>
2022-03-24 14:46:56 +0100

    creo-k-e-acabao2.0

--- src/Views/components/header.php
+++ src/Views/components/header.php
@@ -1,7 +1,7 @@
 <header class="navbar navbar-light container-fluid py-3">
     <a class="navbar-brand d-flex gap-2 align-items-center" href="index.php">
         <img id="logo" src="public/trindrer.png" alt="" width="30" height="30" class="d-inline-block align-text-top">
-        <span class="header-title">Dr Love Liviu</span>
+        <span class="header-title">Dr Liviu Lover</span>
     </a>
     <a href="?action=create" class="btn border-button header-button rounded-circle hover-actions add-hover-actions">
         <i class="fa-solid fa-plus icon"></i>
